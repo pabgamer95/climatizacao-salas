99cdd319323f03ff562cfb5740c9357ca2c3c4fe
pabgamer95 <148779574+pabgamer95@users.noreply.github.com>
2024-10-18 12:07:18 +0100

    deletei comentarios desnecessarios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,38 +5,9 @@ import AdminPage from './pages/AdminPage';
 import ClientPage from './pages/ClientPage';
 import TechnicianPage from './pages/TechnicianPage';
 import PrivateRoute from './components/PrivateRoute';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function App() {
-    /* const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch('https://api.example.com/posts');
-          const result = await response.json();
-          setData(result);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData();
-    }, []); */
-
-
-  /* const [data, setData] = useState();
-  console.log(data)
-
-  useEffect(()=> {
-    fetch('http://localhost:8081/users')
-    .then(res => res.json())
-    .then(data => setData(data))
-    .catch(err => console.log(err))
-  }, []) */
-
-
+ 
   return (
     
     <AuthProvider>
