e3126d8537f5cebadd3c440a1cefe52f1a90b8e2
pabgamer95 <148779574+pabgamer95@users.noreply.github.com>
2024-10-18 13:32:24 +0100

    pegar os utilizadores da database

---
+++ Frontend/src/getBackend/users.jsx
@@ -0,0 +1,32 @@
+    
+import { useState, useEffect } from 'react';
+
+const useUsers = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8081/users');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useUsers;
