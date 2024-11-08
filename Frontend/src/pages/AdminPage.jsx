399c7ad353f992296d750579ee93695384abb382
pabgamer95 <148779574+pabgamer95@users.noreply.github.com>
2024-11-08 13:03:36 +0000

    edit e delete nos utilizadores

--- Frontend/src/pages/AdminPage.jsx
+++ Frontend/src/pages/AdminPage.jsx
@@ -1,21 +1,41 @@
 
-import React from 'react';
-import useUsers from '../getBackend/users';
+import React, {useState} from 'react';
+import axios from 'axios';
+import useUsers from '../Backend/users';
 import  './css/admin.css';
 import './imagens/logoteste.webp';
 
 export default function AdminPage() {
- // const { data, loading, error } = useUsers();
+ const {data, loading, error } = useUsers();
+ const [editId, setEditID] = useState(-1);
+ const [nome, setNome] = useState()
+ const [email ,setEmail] = useState()
+/*  const [newNome, newSetNome] = useState()
+ const [newEmail ,newSetEmail] = useState() */
+ const [role , setRole] = useState()
 
-  ///if (loading) return <p>Loading...</p>;
-  //if (error) return <p>Error fetching data: {error.message}</p>;
-  const users = [
-    { id:1, nome: "Admin", email: "admin@example.com", role: "admin"},
-    { id:2, nome: "Client", email: "client@example.com", role: "client"},
-    { id:3, nome: "Technician", email: "technician@example.com", role: "technician"},
-  ];
+ if (loading) return <p>Loading...</p>;
+ if (error) return <p>Error fetching data: {error.message}</p>;
 
 
+
+
+  const handleEdit = (id) =>{
+    axios.get('http://localhost:8081/users'+id)
+    .then(res => {
+      console.log(res.data)
+      
+    })
+    setEditID(id)
+  }
+
+  const handleUpdate = () => {
+    axios.put('http://localhost:8081/users'+ editId, {id:editId, nome, email, role})
+    .then(res => {
+      console.log(res)
+      setEditID(-1)
+    }).catch(err => console.log(err));
+  }
 
   return (
     
@@ -43,22 +63,33 @@ export default function AdminPage() {
               <th>Role</th>
           </thead>
           <tbody>
-          {users.map((user) => (
-              <tr key={user.id}>
-                <th>{user.id}</th>
-                <th>{user.nome}</th>
-                <th>{user.email}</th>
-                <th>{user.role}</th>
-              </tr>
-            ))}
-           {/*} {data.map((d, i) => (
-              <tr key={i}>
-                <th>{d.id}</th>
-                <th>{d.nome}</th>
-                <th>{d.email}</th>
-                <th>{d.role}</th>
-              </tr> 
-            ))} */}
+            {
+              data.map((d, i) => (
+                d.id === editId ? 
+                <tr>
+                  <th>{d.id}</th>
+                  <th><input type="text" value={d.nome} onChange = {e => setNome(e.target.value)}/></th>
+                  <th><input type="text" value={d.email} onChange = {e => setEmail(e.target.value)}/></th>
+                  <th><select value={d.role} onChange = {e => setRole(e.target.value)}>
+                    <option value="client">Cliente</option>
+                    <option value="technician">Técnico</option>
+                    <option value="admin">Admin</option>
+                </select></th>
+                  <th><button onClick={handleUpdate}>Update</button></th>
+                </tr>
+                :
+                <tr key={i}>
+                  <th>{d.id}</th>
+                  <th>{d.nome}</th>
+                  <th>{d.email}</th>
+                  <th>{d.role}</th>
+                  <th>
+                    <button onClick={() => handleEdit(d.id)}>edit</button>
+                    <button>delete</button>
+                  </th>
+                </tr> 
+              ))
+            }
             
           </tbody>
         </table>
